5c9ea5324f2de3afd78086e8a5343e8b48dbca3c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-01-31 15:44:36 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
-  base: './under-construction',
+  base: '/',
   resolve: {
 	alias: {
 		'@': path.resolve(__dirname, './src')
